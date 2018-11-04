4f7ba18efdf3cdc068face230b31833bca2b413c
Ashley Levi <Ashplevi@gmail.com>
2018-11-03 19:32:54 -0600

    Add method to click next question

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CardContainer from './CardContainer.js';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      allCards: [],
+      counter: 0
+
+  
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('http://memoize-datasets.herokuapp.com/api/v1/prototypeData')
+      .then(response => response.json())
+      .then(allCards =>  {
+        this.setState({
+          allCards: allCards.prototypeData
+        });
+        
+    })
+      .catch(error => console.log(error));
+    }
+
+  switchQuestion = (event) => {
+    if (this.state.counter > 28) {
+    
+    this.setState({
+      counter: this.state.counter = 0
+    })
+  } else {
+    this.setState({
+      counter: this.state.counter+=1  
+    })
   }
 }
+  
+  checkUserAnswer = (answer) => {
+    if (answer === this.state.allCards[this.state.counter].correctAnswer) {
+      console.log('correct')
+    } else {
+      console.log('incorrect')
+    }
+
+
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1>PROTOMASTER</h1>
+          <h2>become a pro at prototypes</h2>
+        </header>
+        <section className="cards">
+        <CardContainer allCards={this.state.allCards} 
+                       count={this.state.counter}
+                       switchQuestion={this.switchQuestion} 
+                       checkUserAnswer={this.checkUserAnswer}/>
+        </section>
+      </div>
+    )
+  }
+}
+
+
+
 
 export default App;
---
+++ src/CardContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Card from './Card.js';
+import './styles/main.scss';
+
+
+export default class CardContainer extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render() { 
+    console.log(this.props)
+    if (!this.props.allCards.length) {
+      return (
+        <div>
+          <p>Loading</p>
+        </div>
+        )
+      } else {
+  
+    return (
+     <div className="cardcontainer-wrapper">
+        <div>
+
+            <Card card={this.props.allCards[this.props.count]}
+                  switchQuestion={this.props.switchQuestion}
+                  checkUserAnswer={this.props.checkUserAnswer} /> 
+          
+        
+        </div>   
+      </div>
+    );
+  }
+}
+}
