04e0d05d4935c59d683572e621e400e95b13448b
Ashley Levi <Ashplevi@gmail.com>
2018-11-03 23:24:11 -0600

    Add css to page

--- src/Card.js
+++ src/Card.js
@@ -11,29 +11,31 @@ export default class Card extends Component {
   }
 
   selectAnswer = (e) => {
-    console.log('answer', e.target.value)
     let answer = e.target.value
     this.setState({
       selectedAnswer: answer
     }) 
+
+
   }
 
   render() { 
     console.log(this.props)
   return (
     <div className="card-wrapper">
+      <i class="fas fa-plus"></i>
         <div className="question-wrapper">
-          <h2>Question: {this.props.card.question}</h2>
+          <h3>Question: {this.props.card.question}</h3>
         </div>
         <div className="answer-bank">
           <ul className="card">{
             this.props.card.answers.map((answer) => {
-              return <li className="answer-choice"><input type="radio" name="answer" value={answer} onClick={e => this.selectAnswer(e)}></input>{answer}</li>
+              return <li className="answer-choice"><input type="radio" name="answer" className="form-radio" label="form" value={answer} onClick={e => this.selectAnswer(e)}></input>{answer}</li>
             })
           }
         </ul>
         </div>
-        <div>
+        <div className="card-buttons">
           <button className="submit-button" onClick={this.props.checkUserAnswer(this.state.selectedAnswer)}>Submit</button>
           <button className="next-button" onClick={this.props.switchQuestion}>Next Question</button>
         </div>  
