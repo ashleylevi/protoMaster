7894b519ba7adf1880d8821618e02191f51f7c73
Ashley Levi <Ashplevi@gmail.com>
2018-11-05 16:21:49 -0700

    Delete from storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import CardContainer from './CardContainer.js';
-
+import React, { Component } from "react";
+import "./App.css";
+import CardContainer from "./CardContainer.js";
 
 class App extends Component {
   constructor() {
@@ -11,108 +10,150 @@ class App extends Component {
       counter: 0,
       lookAtStoredCards: false,
       storedCardIds: []
-    }
+    };
   }
 
   componentDidMount = () => {
-    fetch('http://memoize-datasets.herokuapp.com/api/v1/prototypeData')
+    fetch("http://memoize-datasets.herokuapp.com/api/v1/prototypeData")
       .then(response => response.json())
-      .then(allCards =>  {
+      .then(allCards => {
         this.setState({
           allCards: allCards.prototypeData
         });
-        
-    })
+      })
       .catch(error => console.log(error));
-    }
+  };
 
   displayPage = () => {
-    document.querySelector('.main-card').classList.add('show');
-    document.querySelector('.start-button').classList.add('hide');
-  }
+    document.querySelector(".main-card").classList.add("show");
+    document.querySelector(".start-button").classList.add("hide");
+  };
 
-
-  switchQuestion = (event) => {
+  switchQuestion = event => {
     if (this.state.counter > 28) {
       this.setState({
-        counter: this.state.counter = 0
-      })
+        counter: (this.state.counter = 0)
+      });
     } else {
       this.setState({
-        counter: this.state.counter+=1  
-      })
+        counter: (this.state.counter += 1)
+      });
     }
-  }
+  };
 
-
-  handleState = (storedCards) => {
-    console.log('hi')
-    var storedCards = JSON.parse(localStorage.getItem('clickedCard'))
+  handleState = storedCards => {
+    console.log("hi");
+    var storedCards = JSON.parse(localStorage.getItem("clickedCard"));
     this.setState({
       lookAtStoredCards: !this.state.lookAtStoredCards,
       storedCardIds: storedCards
-    })
-  }
-  
+    });
+  };
+
+
+    // let index = cardsFromStorage.indexOf(card);
+    // cardsFromStorage.splice(index, 1);
+    // this.setState({
+    //   storedCardIds: cardsFromStorage
+    // })
+    //  localStorage.setItem("clickedCard", JSON.stringify(cardsFromStorage))
+
+
+
+
+
+
   render() {
     if (this.state.lookAtStoredCards === true) {
       return (
         <div className="app">
-        <div className="top-right">
-          <p className="account">Account</p>
-          <button className="study-hub" onClick={() => this.handleState()}>HOME</button>
+          <div className="top-right">
+            <p className="account">Account</p>
+            <button className="study-hub" onClick={() => this.handleState()}>
+              HOME
+            </button>
+          </div>
+          <header className="app-header">
+            <h1>
+              PROTO
+              <span>MASTER</span>
+            </h1>
+            <h2>
+              become a <span className="pro">pro</span> at{" "}
+              <span className="pro">proto</span>
+              types
+            </h2>
+          </header>
+          <div className="start">
+            <button className="start-button" onClick={this.displayPage}>
+              Start Studying!
+            </button>
+          </div>
+          <div className="main-card">
+            <section className="cards">
+              <CardContainer
+                allCards={this.state.allCards}
+                count={this.state.counter}
+                switchQuestion={this.switchQuestion}
+                checkUserAnswer={this.checkUserAnswer}
+                lookAtStoredCards={this.state.lookAtStoredCards}
+                storedCardIds={this.state.storedCardIds}
+              />
+            </section>
+          </div>
         </div>
-        <header className="app-header">
-          <h1>PROTO<span>MASTER</span></h1>
-          <h2>become a <span className="pro">pro</span> at <span className="pro">proto</span>types</h2>
-        </header>
-        <div className="start">
-          <button className="start-button" onClick={this.displayPage}>Start Studying!</button>
-        </div>
-        <div className="main-card">
-          <section className="cards">
-          <CardContainer allCards={this.state.allCards} 
-                         count={this.state.counter}
-                         switchQuestion={this.switchQuestion} 
-                         checkUserAnswer={this.checkUserAnswer}
-                         lookAtStoredCards={this.state.lookAtStoredCards}
-                         storedCardIds={this.state.storedCardIds} />
-          </section>  
-        </div>
-    </div>
-    )
-  }
-        
-    
+      );
+    }
+
+      // removeCard = (card) => {
+  //   let cardsFromStorage = JSON.parse(localStorage.getItem("clickedCard"));
+  //   let index = cardsFromStorage.indexOf(card);
+  //   cardsFromStorage.splice(index, 1);
+  //   this.setState({
+  //     storedCardIds: cardsFromStorage
+  //   })
+  //    localStorage.setItem("clickedCard", JSON.stringify(cardsFromStorage))
+  //  }
+
     return (
       <div className="app">
         <div className="top-right">
           <p className="account">Account</p>
-          <button className="study-hub" onClick={() => this.handleState()}>My Study Hub</button>
+          <button className="study-hub" onClick={() => this.handleState()}>
+            My Study Hub
+          </button>
         </div>
         <header className="app-header">
-          <h1>PROTO<span>MASTER</span></h1>
-          <h2>become a <span className="pro">pro</span> at <span className="pro">proto</span>types</h2>
+          <h1>
+            PROTO
+            <span>MASTER</span>
+          </h1>
+          <h2>
+            become a <span className="pro">pro</span> at{" "}
+            <span className="pro">proto</span>
+            types
+          </h2>
         </header>
         <div className="start">
-          <button className="start-button" onClick={this.displayPage}>Start Studying!</button>
+          <button className="start-button" onClick={this.displayPage}>
+            Start Studying!
+          </button>
         </div>
         <div className="main-card">
           <section className="cards">
-          <CardContainer allCards={this.state.allCards} 
-                         count={this.state.counter}
-                         switchQuestion={this.switchQuestion} 
-                         checkUserAnswer={this.checkUserAnswer}
-                         lookAtStoredCards={this.state.lookAtStoredCards}
-                         storedCardIds={this.state.storedCardIds} />
-          </section>  
+            <CardContainer
+              allCards={this.state.allCards}
+              count={this.state.counter}
+              switchQuestion={this.switchQuestion}
+              checkUserAnswer={this.checkUserAnswer}
+              lookAtStoredCards={this.state.lookAtStoredCards}
+              storedCardIds={this.state.storedCardIds}
+            />
+          </section>
         </div>
-    </div>
-    )
+      </div>
+    );
   }
 }
-
-
-
 
 export default App;
--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,72 +1,82 @@
-import React, { Component } from 'react';
-import Card from './Card.js';
-import './styles/main.scss';
-
+import React, { Component } from "react";
+import Card from "./Card.js";
+import "./styles/main.scss";
+import PropTypes from "prop-types";
 
 export default class CardContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       storedCards: []
-    }
-    
+    };
   }
 
-  getStoredCards = (dataFromChild) => {
+  getStoredCards = dataFromChild => {
     this.setState({
       storedCards: dataFromChild
-    })
-  }
+    });
+  };
 
-  render() { 
+  render() {
     if (!this.props.allCards.length) {
       return (
         <div>
           <p>Loading</p>
         </div>
-        )
-      } 
+      );
+    }
 
     if (this.props.allCards.length && this.props.lookAtStoredCards === false) {
-     return (
+      return (
         <div className="cardcontainer-wrapper">
           <div>
-            <Card card={this.props.allCards[this.props.count]}
-                  switchQuestion={this.props.switchQuestion}
-                  checkUserAnswer={this.props.checkUserAnswer}
-                  getStoredCards={this.getStoredCards} /> 
-        </div>   
-      </div>
-    );
-  }
+            <Card
+              card={this.props.allCards[this.props.count]}
+              switchQuestion={this.props.switchQuestion}
+              checkUserAnswer={this.props.checkUserAnswer}
+              getStoredCards={this.getStoredCards}
+            />
+          </div>
+        </div>
+      );
+    }
 
     if (this.props.lookAtStoredCards === true) {
       return (
         <ul>
-          <li>
-            {
-              this.props.storedCardIds.map((id) => {
-                return this.props.allCards.filter((card) => {
-                  return card.id === id
-                })
-              }).reduce((arr, question) => arr.concat(question), []).map(card => {
-                return <Card  card={card} 
-                              key={card.id}
-                              switchQuestion={this.props.switchQuestion}
-                              checkUserAnswer={this.props.checkUserAnswer}
-                              getStoredCards={this.getStoredCards}
-                              lookAtStoredCards={this.props.lookAtStoredCards} /> 
+          <li className="stored-cards">
+            {this.props.storedCardIds
+              .map(id => {
+                return this.props.allCards.filter(card => {
+                  return card.id === id;
+                });
               })
-                
-            }
+              .reduce((arr, question) => arr.concat(question), [])
+              .map(card => {
+                return (
+                  <Card
+                    card={card}
+                    key={card.id}
+                    switchQuestion={this.props.switchQuestion}
+                    checkUserAnswer={this.props.checkUserAnswer}
+                    getStoredCards={this.getStoredCards}
+                    lookAtStoredCards={this.props.lookAtStoredCards}
+                    removeCard={this.props.removeCard}
+                  />
+                );
+              })}
           </li>
         </ul>
-      )
-        
-    }
+      );
     }
   }
+}
 
-
-
-
+CardContainer.propTypes = {
+  allCards: PropTypes.array.isRequired,
+  count: PropTypes.number.isRequired,
+  switchQuestion: PropTypes.func.isRequired,
+  checkUserAnswer: PropTypes.func.isRequired,
+  lookAtStoredCards: PropTypes.func.isRequired,
+  storedCardIds: PropTypes.array.isRequired
+};
